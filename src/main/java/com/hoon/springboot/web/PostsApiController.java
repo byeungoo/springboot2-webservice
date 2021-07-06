06ba8d1c71b37a03ccc7266fda4dd04060bf1aea
byeungoo <dksekfldks65@naver.com>
2021-07-06 20:46:26 +0900

    Post Api 컨트롤러 조회 코드 위치수정

--- src/main/java/com/hoon/springboot/web/PostsApiController.java
+++ src/main/java/com/hoon/springboot/web/PostsApiController.java
@@ -13,6 +13,11 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
@@ -21,11 +26,6 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
-    }
-
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id) {
-        return postsService.findById(id);
     }
 
     @DeleteMapping("/api/v1/posts/{id}")
